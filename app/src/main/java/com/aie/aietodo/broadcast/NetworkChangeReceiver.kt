b0b23dbedcf38049abbc554ab7fc926398c64182
LAPTOP-HCHPS9HP\Samsad <samsad.mastan@gmail.com>
2021-03-08 15:03:04 +0530

    Modified network availability method

--- app/src/main/java/com/aie/aietodo/broadcast/NetworkChangeReceiver.kt
+++ app/src/main/java/com/aie/aietodo/broadcast/NetworkChangeReceiver.kt
@@ -4,6 +4,8 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import android.widget.Toast
 
 /**
@@ -30,19 +32,23 @@ class NetworkChangeReceiver : BroadcastReceiver() {
      * @param : application context
      * @return : boolean
      */
-    fun isNetworkAvailable(context: Context?): Boolean {
-        val connMgr = context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        if (connMgr != null) {
-            val activeNetworkInfo = connMgr.activeNetworkInfo
-            if (activeNetworkInfo != null) { // connected to the internet
-                // connected to the mobile provider's data plan
-                return if (activeNetworkInfo.type == ConnectivityManager.TYPE_WIFI) {
-                    // connected to wifi
-                    true
-                } else
-                    activeNetworkInfo.type == ConnectivityManager.TYPE_MOBILE
+    private fun isNetworkAvailable(context: Context?): Boolean {
+        val connectivityManager = context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val nw      = connectivityManager.activeNetwork ?: return false
+            val actNw = connectivityManager.getNetworkCapabilities(nw) ?: return false
+            return when {
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                //for other device how are able to connect with Ethernet
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                //for check internet over Bluetooth
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_BLUETOOTH) -> true
+                else -> false
             }
+        } else {
+            val nwInfo = connectivityManager.activeNetworkInfo ?: return false
+            return nwInfo.isConnected
         }
-        return false
     }
 }
